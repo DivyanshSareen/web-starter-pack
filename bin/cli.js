041272b8f38b972fcd5196633c5e65c89df35666
DivyanshSareen <sareendivyansh@gmail.com>
2022-10-24 16:40:01 +0530

    feat: add execution command to cli.js

--- bin/cli.js
+++ bin/cli.js
@@ -2,6 +2,16 @@
 
 const {execSync} = require('child_process');
 
+const runCommand = command => {
+    try {
+        execSync(`${command}, {stdio: 'inherit'}`);
+    } catch(e) {
+        console.error(`Failed to execute ${command}`, e);
+        return false;
+    }
+    return true;
+}
+
 const repoName = process.argv[2];
 const gitCheckoutCommand = `git clone --depth 1 https://github.com/DivyanshSareen/web-starter-pack ${repoName}`;
 const installDepsCommand = `cd ${repoName} && npm install`;
