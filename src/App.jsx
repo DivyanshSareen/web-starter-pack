efc59c29178aa8e773b05fffe80be720893fa68b
DivyanshSareen <sareendivyansh@gmail.com>
2022-10-28 13:16:58 +0530

    feat: add react and react-dom, add prettier command

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,7 @@ import "./App.css";
 import Gif from "./giphy.gif";
 
 const App = () => `<div class="message">
-<h2> You are Ready to Code! </h2>
-<img src="${Gif}"/>
+<h2> You are Ready to Code! </h2> <img src="${Gif}"/>
 <a target="_blank" href="https://github.com/DivyanshSareen/web-starter-pack">click to learn more</a>
 </div`;
 export default App;
