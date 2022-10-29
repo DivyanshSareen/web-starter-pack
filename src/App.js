e5d3e772014fdf9f3105e34341cd73692e203559
DivyanshSareen <sareendivyansh@gmail.com>
2022-10-29 13:33:32 +0530

    feat: add new favicon, add react components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+import React from "react";
 import Gif from "./giphy.gif";
 
 const App = () => {
--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import ReactDom from "react-dom"
 import App from "./App"
 import "./App.css"
 
-ReactDom.render(<App />, document.getElementById('app'))
+ReactDom.render(<><App/></>, document.getElementById('root'))
